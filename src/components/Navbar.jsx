f4900d13186e06c3d21c17eae39fdce505a72b5d
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2021-12-15 12:20:27 +0200

    navbar is coplated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,44 @@
 import React, {useState} from 'react';
+import pic from '../assets/logo.png';
+import navPic from '../assets/nav-pic.jpg'
+import {MdHomeFilled} from 'react-icons/md';
+import {BiMessageRoundedDots} from 'react-icons/bi';
+import {BiMessageSquareAdd} from 'react-icons/bi';
+import {IoCompassOutline} from 'react-icons/io5';
+import {FiHeart} from 'react-icons/fi';
 import './navbar.css'
 
 function Navbar() {
     return(
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRVL
-                    </Link>
-                </div>
-            </nav>
-        </>
+       <div className="nav">
+           <ul className="nav__list">
+               <li className="nav__link nav__link--logo">
+                <img src={pic}  alt='logo'/>
+               </li>
+               <li className="nav__link nav__link--search">
+                   <input type="search" placeholder="Search"/>
+               </li>
+                <li className="nav__link nav__link--icon">
+                    <MdHomeFilled/>
+                </li>
+                <li className="nav__link nav__link--icon">
+                    <BiMessageRoundedDots/>
+                </li>
+                <li className="nav__link nav__link--icon">
+                    <BiMessageSquareAdd/>
+                </li>
+                <li className="nav__link nav__link--icon">
+                    <IoCompassOutline/>
+                </li>
+                <li className="nav__link nav__link--icon">
+                    <FiHeart/>
+                </li>
+                <li className="nav__link nav__link--img">
+                    <img src={navPic}  alt="navPic" />
+                </li>
+
+           </ul>
+       </div>
     )
 };
 export default Navbar;
