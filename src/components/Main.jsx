2258a302c36fe8663bd318cd76057c917e10c955
Niyazova_Gulnoza <niyazovagulnoza2@gmail.com>
2021-12-15 12:20:00 +0200

    nabar coplated

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+function Main() {
+    return (
+       <main>
+           <div className="main__left">
+                <div className="main__top">
+                    
+                </div>
+                <div className="main__bottom">
+
+                </div>
+           </div>
+           <div className="main__right">
+
+           </div>
+       </main>
+    )
+}
+
+export default Main;
